3d49fd5c4c59384885d3e707d83d3c21a76f3347
ThomasSKokholm <ThomasSKokholm@gmail.com>
2020-03-05 22:17:56 +0100

    Starting on db models.

--- app/models.py
+++ app/models.py
@@ -7,3 +7,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login_manager
 
 
+class Humanbeing(UserMixin, db.Model):
+    """Create an Human being table"""
+    __tablename__ = 'humanbeings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), index=True, unique=True)
+    callname = db.Column(db.String(255), index=True, unique=True)
+    first_name = db.Column(db.String(255), index=True)
+    last_name = db.Column(db.String(255), index=True)
+    password_hash = db.Column(db.String(128))
+    
+
