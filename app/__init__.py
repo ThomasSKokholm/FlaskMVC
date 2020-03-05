1de94348de05293f2e5d8ba5dcfe2af87228b346
ThomasSKokholm <ThomasSKokholm@gmail.com>
2020-03-05 20:47:53 +0100

    Some login manager code.

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,16 @@
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+<<<<<<< Updated upstream
 from flask_bootstrap import Bootstrap
+=======
+from flask_login import LoginManager
+>>>>>>> Stashed changes
 
 from config import app_config
-from app import views
 
 db = SQLAlchemy()
-
+login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True, static_url_path='/static')
@@ -29,5 +32,9 @@ def create_app(config_name):
     @app.route('/about')
     def about():
         return render_template("about.html")
+
+    login_manager.init_app(app)
+    login_manager.login_message = "Login, before accessing this page."
+    login_manager.login_view = "auth.login"
 
     return app
---
+++ app/models.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db, login_manager
+
+
