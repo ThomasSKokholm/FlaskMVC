39c093e69a78a0e670c4e37f4be7a9320c7d6e73
Thomas S. Kokholm <ThomasSKokholm@gmail.com>
2020-03-05 13:25:09 +0100

    Added some bootstrap jumbotron templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 from config import app_config
+from app import views
 
 db = SQLAlchemy()
 
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_url_path='/static')
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('../config.py')
     db.init_app(app)
+    bootstrap = Bootstrap(app)
+
+    @app.route('/404')
+    def index_404():
+        return "Don't PANIC!"
+
+    @app.route('/')
+    def index():
+        return render_template("index.html")
+
+    @app.route('/about')
+    def about():
+        return render_template("about.html")
 
     return app
